874deaaa87830a10051980ff915314dc016ec2df
jeural <jijihecaoxiaoye@gmail.com>
2023-11-17 11:07:10 +0800

    短链接后管 Redis 常量类

---
+++ admin/src/main/java/com/jane/shortlink/admin/common/constant/RedisCacheConstant.java
@@ -0,0 +1,8 @@
+package com.jane.shortlink.admin.common.constant;
+
+/**
+ * _@Description: 短链接后管 Redis 常量类
+ */
+public class RedisCacheConstant {
+    public static final String LOCK_USER_REGISTER_KEY = "short-link:lock_user-register:";
+}
