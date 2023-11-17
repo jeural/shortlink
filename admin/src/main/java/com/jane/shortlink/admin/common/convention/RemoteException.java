7958220776be19edd6062631d9016fe9dcd646d1
jeural <jijihecaoxiaoye@gmail.com>
2023-11-17 11:03:11 +0800

    异常处理

---
+++ admin/src/main/java/com/jane/shortlink/admin/common/convention/RemoteException.java
@@ -0,0 +1,31 @@
+package com.jane.shortlink.admin.common.convention;
+
+import com.jane.shortlink.admin.common.convention.errorcode.BaseErrorCode;
+import com.jane.shortlink.admin.common.convention.errorcode.IErrorCode;
+import com.jane.shortlink.admin.common.convention.exception.AbstractException;
+
+/**
+ * _@Description: 远程服务调用异常
+ */
+public class RemoteException extends AbstractException {
+
+    public RemoteException(String message) {
+        this(message, null, BaseErrorCode.REMOTE_ERROR);
+    }
+
+    public RemoteException(String message, IErrorCode errorCode) {
+        this(message, null, errorCode);
+    }
+
+    public RemoteException(String message, Throwable throwable, IErrorCode errorCode) {
+        super(message, throwable, errorCode);
+    }
+
+    @Override
+    public String toString() {
+        return "RemoteException{" +
+                "code='" + errorCode + "'," +
+                "message='" + errorMessage + "'" +
+                '}';
+    }
+}
---
+++ admin/src/main/java/com/jane/shortlink/admin/common/convention/errorcode/IErrorCode.java
@@ -0,0 +1,17 @@
+package com.jane.shortlink.admin.common.convention.errorcode;
+
+/**
+ * _@Description: 平台错误码
+ */
+public interface IErrorCode {
+
+    /**
+     * 错误码
+     */
+    String code();
+
+    /**
+     * 错误信息
+     */
+    String message();
+}
