32000163dfc906452a3506507c0b5ce3a068b8c5
jeural <jijihecaoxiaoye@gmail.com>
2023-11-17 11:17:15 +0800

    用户脱敏信息

--- admin/src/main/java/com/jane/shortlink/admin/dto/resp/UserRespDto.java
+++ admin/src/main/java/com/jane/shortlink/admin/dto/resp/UserRespDto.java
@@ -1,5 +1,7 @@
 package com.jane.shortlink.admin.dto.resp;
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.jane.shortlink.admin.common.serialize.PhoneDesensitizationSerializer;
 import lombok.Data;
 
 /**
@@ -10,6 +12,7 @@ public class UserRespDto {
     private Long id;
     private String username;
     private String realName;
+    @JsonSerialize(using = PhoneDesensitizationSerializer.class)
     private String phone;
     private String mail;
 }
