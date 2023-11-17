fb2e547939a934d6a3c14e7844ccd534cab2eeaa
jeural <jijihecaoxiaoye@gmail.com>
2023-11-17 11:12:39 +0800

    UserMapper接口

---
+++ admin/src/main/java/com/jane/shortlink/admin/dao/mapper/UserMapper.java
@@ -0,0 +1,11 @@
+package com.jane.shortlink.admin.dao.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.jane.shortlink.admin.dao.entity.UserDo;
+
+/**
+ * _@Description: user
+ */
+public interface UserMapper extends BaseMapper<UserDo> {
+
+}
