71980288b72977d8e29aa2090e212764106141d8
jeural <jijihecaoxiaoye@gmail.com>
2023-11-17 11:12:49 +0800

    用户注册请求参数

---
+++ admin/src/main/java/com/jane/shortlink/admin/dto/req/UserRegisterReqDto.java
@@ -0,0 +1,15 @@
+package com.jane.shortlink.admin.dto.req;
+
+import lombok.Data;
+
+/**
+ * _@Description: 用户注册请求参数
+ */
+@Data
+public class UserRegisterReqDto {
+    private Long id;
+    private String username;
+    private String realName;
+    private String phone;
+    private String mail;
+}
