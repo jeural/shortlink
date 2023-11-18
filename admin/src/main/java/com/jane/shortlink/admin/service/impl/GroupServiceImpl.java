f0a971267c6c88ef5ed03da1ad092201aab627a1
jeural <jijihecaoxiaoye@gmail.com>
2023-11-18 20:59:43 +0800

    短链接分组接口实现层

---
+++ admin/src/main/java/com/jane/shortlink/admin/service/impl/GroupServiceImpl.java
@@ -0,0 +1,50 @@
+package com.jane.shortlink.admin.service.impl;
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.jane.shortlink.admin.dao.entity.GroupDO;
+import com.jane.shortlink.admin.dao.mapper.GroupMapper;
+import com.jane.shortlink.admin.service.GroupService;
+import com.jane.shortlink.admin.toolkit.RandomGenerator;
+import groovy.util.logging.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ * _@Description: 短链接分组接口实现层
+ */
+@Slf4j
+@Service
+public class GroupServiceImpl extends ServiceImpl<GroupMapper, GroupDO> implements GroupService {
+
+    /**
+     * 新增短链接分组
+     */
+    @Override
+    public void savaGroup(String groupName) {
+        String gid;
+        do {
+            gid = RandomGenerator.generateRandom();
+        } while (!hasGid(gid));
+        // 链式构造器注入
+        GroupDO groupDO = GroupDO.builder()
+                .gid(gid)
+                .name(groupName)
+                .build();
+        baseMapper.insert(groupDO);
+    }
+
+    /**
+     * 查询短链接分组名是否存在 true-不存在 可创建
+     */
+    public Boolean hasGid(String gid) {
+        // 避免重复命名
+        gid = RandomGenerator.generateRandom();
+        LambdaQueryWrapper<GroupDO> queryWrapper = Wrappers.lambdaQuery(GroupDO.class)
+                .eq(GroupDO::getGid, gid)
+                //TODO: 传入用户名
+                .eq(GroupDO::getUsername, null);
+        GroupDO hasGroupFlag = baseMapper.selectOne(queryWrapper);
+        return hasGroupFlag == null;
+    }
+}
