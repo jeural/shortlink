f5c98e276492d118500f113faa38b51c706324db
jeural <jijihecaoxiaoye@gmail.com>
2023-11-18 14:37:55 +0800

    用户注册请求参数

--- admin/src/main/java/com/jane/shortlink/admin/dto/req/UserRegisterReqDTO.java
+++ admin/src/main/java/com/jane/shortlink/admin/dto/req/UserRegisterReqDTO.java
@@ -6,7 +6,7 @@ import lombok.Data;
  * _@Description: 用户注册请求参数
  */
 @Data
-public class UserRegisterReqDto {
+public class UserRegisterReqDTO {
     private Long id;
     private String username;
     private String realName;
