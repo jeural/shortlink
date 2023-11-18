8ddbbd26357273b30a2dce0a17dce3b17f32386f
jeural <jijihecaoxiaoye@gmail.com>
2023-11-18 21:02:25 +0800

    用户信息持久层

--- admin/src/main/java/com/jane/shortlink/admin/dao/mapper/UserMapper.java
+++ admin/src/main/java/com/jane/shortlink/admin/dao/mapper/UserMapper.java
@@ -1,11 +1,11 @@
 package com.jane.shortlink.admin.dao.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.jane.shortlink.admin.dao.entity.UserDo;
+import com.jane.shortlink.admin.dao.entity.UserDO;
 
 /**
- * _@Description: user
+ * _@Description: 用户信息持久层
  */
-public interface UserMapper extends BaseMapper<UserDo> {
+public interface UserMapper extends BaseMapper<UserDO> {
 
 }
