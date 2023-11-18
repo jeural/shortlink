34d30b01bc4331cfb59098b4c7d0b8b59c3a0d25
jeural <jijihecaoxiaoye@gmail.com>
2023-11-18 20:59:22 +0800

    短链接分组持久层

---
+++ admin/src/main/java/com/jane/shortlink/admin/dao/mapper/GroupMapper.java
@@ -0,0 +1,10 @@
+package com.jane.shortlink.admin.dao.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.jane.shortlink.admin.dao.entity.GroupDO;
+
+/**
+ * _@Description: 短链接分组持久层
+ */
+public interface GroupMapper extends BaseMapper<GroupDO> {
+}
