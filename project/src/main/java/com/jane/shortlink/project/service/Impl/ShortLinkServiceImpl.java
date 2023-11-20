8f1cf20ce26b16c7be05ada391cd60b18c87a689
jeural <jijihecaoxiaoye@gmail.com>
2023-11-20 16:08:43 +0800

    实现创建短链接功能

---
+++ project/src/main/java/com/jane/shortlink/project/service/Impl/ShortLinkServiceImpl.java
@@ -0,0 +1,90 @@
+package com.jane.shortlink.project.service.Impl;
+
+import cn.hutool.core.text.StrBuilder;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.jane.shortlink.project.common.convention.ServiceException;
+import com.jane.shortlink.project.dao.entity.ShortLinkDO;
+import com.jane.shortlink.project.dao.mapper.ShortLinkMapper;
+import com.jane.shortlink.project.dto.req.ShortLinkCreateReqDTO;
+import com.jane.shortlink.project.dto.resp.ShortLinkCreateRespDTO;
+import com.jane.shortlink.project.service.ShortLinkService;
+import com.jane.shortlink.project.toolkit.HashUtil;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.redisson.api.RBloomFilter;
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.stereotype.Service;
+
+/**
+ * _@Description: 短链接接口实现层
+ */
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class ShortLinkServiceImpl extends ServiceImpl<ShortLinkMapper, ShortLinkDO> implements ShortLinkService {
+
+    private final RBloomFilter<String> shortUriCreateCachePenetrationBloomFilter;
+
+    /**
+     * 创建短链接
+     */
+    @Override
+    public ShortLinkCreateRespDTO createShortLink(ShortLinkCreateReqDTO requestParam) {
+        String shortLinkSuffix = generateSuffix(requestParam);
+        String fullShortUrl = StrBuilder.create(requestParam.getDomain())
+                .append("/")
+                .append(shortLinkSuffix)
+                .toString();
+        ShortLinkDO shortLinkDO = ShortLinkDO.builder()
+                .domain(requestParam.getDomain())
+                .originUrl(requestParam.getOriginUrl())
+                .gid(requestParam.getGid())
+                .createType(requestParam.getCreateType())
+                .validDateType(requestParam.getValidDateType())
+                .validDate(requestParam.getValidDate())
+                .describe(requestParam.getDescribe())
+                .shortUri(shortLinkSuffix)
+                .enableStatus(0)
+                .fullShortUrl(fullShortUrl)
+                .build();
+        try {
+            baseMapper.insert(shortLinkDO);
+        } catch (DuplicateKeyException e) {
+            // 防止误判
+            LambdaQueryWrapper<ShortLinkDO> queryWrapper = Wrappers.lambdaQuery(ShortLinkDO.class)
+                    .eq(ShortLinkDO::getFullShortUrl, fullShortUrl);
+            ShortLinkDO hasshortLinkDO= baseMapper.selectOne(queryWrapper);
+            if (hasshortLinkDO != null) {
+                log.warn("短链接：{} 重复入库", fullShortUrl);
+                throw new ServiceException("短链接生成重复");
+            }
+        }
+        shortUriCreateCachePenetrationBloomFilter.add(shortLinkSuffix);
+        return ShortLinkCreateRespDTO.builder()
+                .fullShortUrl(shortLinkDO.getFullShortUrl())
+                .originUrl(shortLinkDO.getOriginUrl())
+                .gid(requestParam.getGid())
+                .build();
+    }
+
+    private String generateSuffix(ShortLinkCreateReqDTO requestParam) {
+        int customGenerateCount = 0;
+        String shortUri;
+        while (true) {
+            if (customGenerateCount > 10) {
+                throw new ServiceException("短链接频繁生成，请稍后再试");
+            }
+            String originUrl = requestParam.getOriginUrl();
+            // 同一个原链接生成的 shortUri 是相同的
+            originUrl += System.currentTimeMillis();
+            shortUri = HashUtil.hashToBase62(originUrl);
+            if (!shortUriCreateCachePenetrationBloomFilter.contains(requestParam.getDomain() + "/" + shortUri)) {
+                break;
+            }
+            customGenerateCount++;
+        }
+        return shortUri;
+    }
+}
---
+++ project/src/main/java/com/jane/shortlink/project/service/ShortLinkService.java
@@ -0,0 +1,19 @@
+package com.jane.shortlink.project.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.jane.shortlink.project.dao.entity.ShortLinkDO;
+import com.jane.shortlink.project.dto.req.ShortLinkCreateReqDTO;
+import com.jane.shortlink.project.dto.resp.ShortLinkCreateRespDTO;
+
+/**
+ * _@Description: 短链接接口层
+ */
+public interface ShortLinkService extends IService<ShortLinkDO> {
+
+    /**
+     * 创建短链接
+     * @param requestParam 创建短链接请求参数
+     * @return 短链接创建信息
+     */
+    ShortLinkCreateRespDTO createShortLink(ShortLinkCreateReqDTO requestParam);
+}
