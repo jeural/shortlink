058002d6b482f0c14b1f32012118e9e7bd45db8f
jeural <jijihecaoxiaoye@gmail.com>
2023-11-20 15:59:21 +0800

    短链接持久层

---
+++ project/src/main/java/com/jane/shortlink/project/dao/mapper/ShortLinkMapper.java
@@ -0,0 +1,10 @@
+package com.jane.shortlink.project.dao.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.jane.shortlink.project.dao.entity.ShortLinkDO;
+
+/**
+ * _@Description: 短链接持久层
+ */
+public interface ShortLinkMapper extends BaseMapper<ShortLinkDO> {
+}
