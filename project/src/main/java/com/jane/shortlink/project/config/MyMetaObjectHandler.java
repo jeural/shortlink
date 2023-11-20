9fc79a0a6aaf03d2ba9c924fe09503a9d7584da2
jeural <jijihecaoxiaoye@gmail.com>
2023-11-20 16:12:30 +0800

    project BaseDO 数据自动填充

---
+++ project/src/main/java/com/jane/shortlink/project/config/MyMetaObjectHandler.java
@@ -0,0 +1,31 @@
+package com.jane.shortlink.project.config;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDateTime;
+
+/**
+ * _@Description: 用户创建/更新时间、数据状态自动填充
+ */
+
+@Slf4j
+@Configuration
+public class MyMetaObjectHandler implements MetaObjectHandler {
+
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        log.info("start insert fill ....");
+        this.strictInsertFill(metaObject, "createTime", LocalDateTime::now, LocalDateTime.class);
+        this.strictInsertFill(metaObject, "updateTime", LocalDateTime::now, LocalDateTime.class);
+        this.strictInsertFill(metaObject, "delFlag", () -> 0, Integer.class);
+    }
+
+    @Override
+    public void updateFill(MetaObject metaObject) {
+        log.info("start update fill ....");
+        this.strictUpdateFill(metaObject, "updateTime", LocalDateTime::now, LocalDateTime.class);
+    }
+}
