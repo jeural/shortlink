57fa3ae9c13ee17623ba9ec462783d6d527292bd
jeural <jijihecaoxiaoye@gmail.com>
2023-11-21 10:02:20 +0800

    短链接实体类新增网站图标

--- project/src/main/java/com/jane/shortlink/project/dao/entity/ShortLinkDO.java
+++ project/src/main/java/com/jane/shortlink/project/dao/entity/ShortLinkDO.java
@@ -56,6 +56,11 @@ public class ShortLinkDO extends BaseDO {
     private String gid;
 
     /**
+     * 网站图标
+     */
+    private String favicon;
+
+    /**
      * 启用标识 0：启用 1：未启用
      */
     private Integer enableStatus;
@@ -81,10 +86,7 @@ public class ShortLinkDO extends BaseDO {
     @TableField("`describe`")
     private String describe;
 
-//    /**
-//     * 网站标识
-//     */
-//    private String favicon;
+
 //
 //    /**
 //     * 历史PV
