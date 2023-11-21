e566d27d760e326ae3d7aee4677adbaf6becb1db
jeural <jijihecaoxiaoye@gmail.com>
2023-11-21 19:52:52 +0800

    新增查询短链接分组内数量功能

--- project/src/main/java/com/jane/shortlink/project/service/ShortLinkService.java
+++ project/src/main/java/com/jane/shortlink/project/service/ShortLinkService.java
@@ -6,7 +6,10 @@ import com.jane.shortlink.project.dao.entity.ShortLinkDO;
 import com.jane.shortlink.project.dto.req.ShortLinkCreateReqDTO;
 import com.jane.shortlink.project.dto.req.ShortLinkPageReqDTO;
 import com.jane.shortlink.project.dto.resp.ShortLinkCreateRespDTO;
+import com.jane.shortlink.project.dto.resp.ShortLinkGroupCountQueryRespDTO;
 import com.jane.shortlink.project.dto.resp.ShortLinkPageRespDTO;
+
+import java.util.List;
 
 /**
  * _@Description: 短链接接口层
@@ -26,4 +29,11 @@ public interface ShortLinkService extends IService<ShortLinkDO> {
      * @return 短链接分页查询返回结果
      */
     IPage<ShortLinkPageRespDTO> pageShortLink(ShortLinkPageReqDTO requestParam);
+
+    /**
+     * 查询短链接分组内数量
+     * @param requestParam 查询短链接分组内数量请求参数
+     * @return 查询短链接分组内数量响应
+     */
+    List<ShortLinkGroupCountQueryRespDTO> listGroupShortLinkCount(List<String> requestParam);
 }
