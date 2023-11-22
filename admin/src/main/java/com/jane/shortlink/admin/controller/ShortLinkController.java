6aeaf28de79c259a1c995f0471aa54064c53a04c
jeural <jijihecaoxiaoye@gmail.com>
2023-11-22 16:02:44 +0800

    短链接后管新增短链接修改功能

--- admin/src/main/java/com/jane/shortlink/admin/controller/ShortLinkController.java
+++ admin/src/main/java/com/jane/shortlink/admin/controller/ShortLinkController.java
@@ -2,15 +2,14 @@ package com.jane.shortlink.admin.controller;
 
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.jane.shortlink.admin.common.convention.result.Result;
+import com.jane.shortlink.admin.common.convention.result.Results;
 import com.jane.shortlink.admin.remote.dto.ShortLinkRemoteService;
 import com.jane.shortlink.admin.remote.dto.req.ShortLinkCreateReqDTO;
 import com.jane.shortlink.admin.remote.dto.req.ShortLinkPageReqDTO;
+import com.jane.shortlink.admin.remote.dto.req.ShortLinkUpdateReqDTO;
 import com.jane.shortlink.admin.remote.dto.resp.ShortLinkCreateRespDTO;
 import com.jane.shortlink.admin.remote.dto.resp.ShortLinkPageRespDTO;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * _@Description: 短链接后管控制层
@@ -33,10 +32,19 @@ public class ShortLinkController {
     }
 
     /**
+     * 修改短链接
+     */
+    @PostMapping("/api/short-link/admin/v1/update")
+    public Result<Void> updateShortLink(@RequestBody ShortLinkUpdateReqDTO requestParam) {
+        shortLinkRemoteService.updateShortLink(requestParam);
+        return Results.success();
+    }
+
+    /**
      * 短链接分页查询
      */
     @GetMapping("/api/short-link/admin/v1/page")
-    public Result<IPage<ShortLinkPageRespDTO>> pageShortLink(ShortLinkPageReqDTO requestParam) {
+    public Result<IPage<ShortLinkPageRespDTO>> pageShortLink(@RequestBody ShortLinkPageReqDTO requestParam) {
         return shortLinkRemoteService.pageShortLink(requestParam);
     }
 }
