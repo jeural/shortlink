154ce4e6c9427d65285fdda225548a63c6cfb3d0
jeural <jijihecaoxiaoye@gmail.com>
2023-11-25 18:13:28 +0800

    新增短链接回收站分页查询功能

--- admin/src/main/java/com/jane/shortlink/admin/controller/RecycleBinController.java
+++ admin/src/main/java/com/jane/shortlink/admin/controller/RecycleBinController.java
@@ -1,9 +1,15 @@
 package com.jane.shortlink.admin.controller;
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.jane.shortlink.admin.common.convention.result.Result;
 import com.jane.shortlink.admin.common.convention.result.Results;
 import com.jane.shortlink.admin.remote.dto.ShortLinkRemoteService;
 import com.jane.shortlink.admin.remote.dto.req.RecycleBinSaveRepDTO;
+import com.jane.shortlink.admin.remote.dto.req.ShortLinkRecycleBinPageReqDTO;
+import com.jane.shortlink.admin.remote.dto.resp.ShortLinkPageRespDTO;
+import com.jane.shortlink.admin.service.RecycleBinService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,7 +17,10 @@ import org.springframework.web.bind.annotation.RestController;
  * _@Description: 回收站管理控制层
  */
 @RestController
+@RequiredArgsConstructor
 public class RecycleBinController {
+
+    private final RecycleBinService recycleBinService;
 
     /**
      * TODO: 后续重构为 SpringCloud Feign 调用
@@ -23,5 +32,13 @@ public class RecycleBinController {
     public Result<Void> saveRecycleBin(RecycleBinSaveRepDTO requestParam) {
         shortLinkRemoteService.saveRecycleBin(requestParam);
         return Results.success();
+    }
+
+    /**
+     * 分页查询回收站短链接
+     */
+    @GetMapping("/api/short-link/admin/v1/recycle-bin/page")
+    public Result<IPage<ShortLinkPageRespDTO>> pageShortLink(ShortLinkRecycleBinPageReqDTO requestParam) {
+        return recycleBinService.pageRecycleBinShortLink(requestParam);
     }
 }
