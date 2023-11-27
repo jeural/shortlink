5ca1427ffe00f7ee6015890912913da75fc3e87b
jeural <jijihecaoxiaoye@gmail.com>
2023-11-27 12:01:00 +0800

    新增短链接监控功能-基础访问UIP数据

--- project/src/main/java/com/jane/shortlink/project/toolkit/LinkUtil.java
+++ project/src/main/java/com/jane/shortlink/project/toolkit/LinkUtil.java
@@ -1,6 +1,7 @@
 package com.jane.shortlink.project.toolkit;
 
 import cn.hutool.core.date.LocalDateTimeUtil;
+import jakarta.servlet.http.HttpServletRequest;
 
 import java.time.LocalDateTime;
 import java.util.Optional;
@@ -21,5 +22,30 @@ public class LinkUtil {
         return Optional.ofNullable(validDate)
                 .map(each -> LocalDateTimeUtil.between(LocalDateTime.now(), each).toMillis())
                 .orElse(DEFAULT_CACHE_VALID_TIME);
+    }
+
+    /**
+     * 获取用户真实IP
+     * @param request 请求
+     * @return 用户真实IP
+     */
+    public static String getActualIp(HttpServletRequest request) {
+        String ipAddress = request.getHeader("X-Forwarded-For");
+        if (ipAddress == null || ipAddress.isEmpty() || "unknown".equalsIgnoreCase(ipAddress)) {
+            ipAddress = request.getHeader("Proxy-Client-IP");
+        }
+        if (ipAddress == null || ipAddress.isEmpty() || "unknown".equalsIgnoreCase(ipAddress)) {
+            ipAddress = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if (ipAddress == null || ipAddress.isEmpty() || "unknown".equalsIgnoreCase(ipAddress)) {
+            ipAddress = request.getHeader("HTTP_CLIENT_IP");
+        }
+        if (ipAddress == null || ipAddress.isEmpty() || "unknown".equalsIgnoreCase(ipAddress)) {
+            ipAddress = request.getHeader("HTTP_X_FORWARDED_FOR");
+        }
+        if (ipAddress == null || ipAddress.isEmpty() || "unknown".equalsIgnoreCase(ipAddress)) {
+            ipAddress = request.getRemoteAddr();
+        }
+        return ipAddress;
     }
 }
