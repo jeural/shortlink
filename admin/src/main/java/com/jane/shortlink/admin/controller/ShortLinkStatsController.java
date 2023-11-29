4fd3f4e68652dc1db29b02d3627ecbea99aff194
jeural <jijihecaoxiaoye@gmail.com>
2023-11-29 21:32:19 +0800

    新增短链接后管调用监接口情功能

---
+++ admin/src/main/java/com/jane/shortlink/admin/controller/ShortLinkStatsController.java
@@ -0,0 +1,31 @@
+package com.jane.shortlink.admin.controller;
+
+import com.jane.shortlink.admin.common.convention.result.Result;
+import com.jane.shortlink.admin.remote.dto.ShortLinkRemoteService;
+import com.jane.shortlink.admin.remote.dto.req.ShortLinkStatsReqDTO;
+import com.jane.shortlink.admin.remote.dto.resp.ShortLinkStatsRespDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * _@Description: 短链接监控控制层
+ */
+@RestController
+@RequiredArgsConstructor
+public class ShortLinkStatsController {
+
+    /**
+     * TODO:后续重构为 SpringCloud Feign 调用
+     */
+    ShortLinkRemoteService shortLinkRemoteService = new ShortLinkRemoteService() {
+    };
+
+    /**
+     * 访问单个短链接指定时间内监控数据
+     */
+    @GetMapping("/api/short-link/admin/v1/stats")
+    public Result<ShortLinkStatsRespDTO> shortLinkStats(ShortLinkStatsReqDTO requestParam) {
+        return shortLinkRemoteService.oneShortLinkStats(requestParam);
+    }
+}
