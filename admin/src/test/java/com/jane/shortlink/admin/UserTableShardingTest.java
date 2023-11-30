267387dc70a10c255486fc918a8b5b82fb7af2fe
jeural <jijihecaoxiaoye@gmail.com>
2023-11-30 19:22:22 +0800

    开发短链接分页监控相关功能

--- admin/src/test/java/com/jane/shortlink/admin/UserTableShardingTest.java
+++ admin/src/test/java/com/jane/shortlink/admin/UserTableShardingTest.java
@@ -4,18 +4,20 @@ package com.jane.shortlink.admin;
  * _@Description:
  */
 public class UserTableShardingTest {
-    public static final String SQL = "CREATE TABLE `t_group_%d` (\n" +
-            "  `id` bigint NOT NULL AUTO_INCREMENT COMMENT 'ID',\n" +
-            "  `gid` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '分组标识',\n" +
-            "  `name` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '分组名称',\n" +
-            "  `username` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '创建分组用户名',\n" +
-            "  `sort_order` int NULL DEFAULT NULL COMMENT '分组排序',\n" +
-            "  `create_time` datetime NULL DEFAULT NULL COMMENT '创建时间',\n" +
-            "  `update_time` datetime NULL DEFAULT NULL COMMENT '修改时间',\n" +
-            "  `del_flag` tinyint(1) NULL DEFAULT NULL,\n" +
-            "  PRIMARY KEY (`id`) USING BTREE,\n" +
-            "  UNIQUE INDEX `idx_unique_username_gid`(`gid` ASC, `username` ASC) USING BTREE\n" +
-            ") ENGINE = InnoDB AUTO_INCREMENT = 1726163680434348034 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;\n";
+    public static final String SQL = "CREATE TABLE `t_link_stats_today_%d` (\n" +
+            "  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'ID',\n" +
+            "  `gid` varchar(32) DEFAULT 'default' COMMENT '分组标识',\n" +
+            "  `full_short_url` varchar(128) DEFAULT NULL COMMENT '短链接',\n" +
+            "  `date` date DEFAULT NULL COMMENT '日期',\n" +
+            "  `today_pv` int(11) DEFAULT '0' COMMENT '今日PV',\n" +
+            "  `today_uv` int(11) DEFAULT '0' COMMENT '今日UV',\n" +
+            "  `today_uip` int(11) DEFAULT '0' COMMENT '今日IP数',\n" +
+            "  `create_time` datetime DEFAULT NULL COMMENT '创建时间',\n" +
+            "  `update_time` datetime DEFAULT NULL COMMENT '修改时间',\n" +
+            "  `del_flag` tinyint(1) DEFAULT NULL COMMENT '删除标识 0：未删除 1：已删除',\n" +
+            "  PRIMARY KEY (`id`),\n" +
+            "  UNIQUE KEY `idx_unique_full-short-url` (`full_short_url`) USING BTREE\n" +
+            ") ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;;";
 
     public static void main(String[] args) {
         for (int i = 0; i < 16; i++) {
---
+++ project/src/main/java/com/jane/shortlink/project/dao/mapper/LinkStatsTodayMapper.java
@@ -0,0 +1,10 @@
+package com.jane.shortlink.project.dao.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.jane.shortlink.project.dao.entity.LinkStatsTodayDO;
+
+/**
+ * _@Description: 短链接今日统计持久层
+ */
+public interface LinkStatsTodayMapper extends BaseMapper<LinkStatsTodayDO> {
+}
