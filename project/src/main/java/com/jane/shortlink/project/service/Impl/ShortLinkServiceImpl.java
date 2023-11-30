a7364fe52daa45b21043a23846511948580820ca
jeural <jijihecaoxiaoye@gmail.com>
2023-11-30 20:16:58 +0800

    新增短链接监控汇总统计

--- project/src/main/java/com/jane/shortlink/project/service/Impl/ShortLinkServiceImpl.java
+++ project/src/main/java/com/jane/shortlink/project/service/Impl/ShortLinkServiceImpl.java
@@ -101,7 +101,7 @@ public class ShortLinkServiceImpl extends ServiceImpl<ShortLinkMapper, ShortLink
                 .describe(requestParam.getDescribe())
                 .shortUri(shortLinkSuffix)
                 .enableStatus(0)
-                .totalUv(0)
+                .totalPv(0)
                 .totalUv(0)
                 .totalUip(0)
                 .fullShortUrl(fullShortUrl)
@@ -399,6 +399,7 @@ public class ShortLinkServiceImpl extends ServiceImpl<ShortLinkMapper, ShortLink
                         .fullShortUrl(fullShortUrl)
                         .build();
                 linkAccessLogsMapper.insert(linkAccessLogsDO);
+                baseMapper.incrementStats(gid, fullShortUrl, 1, uvFirstFlag.get() ? 1 : 0, uipFirstFlag ? 1 : 0);
             }
         } catch (Throwable ex) {
             log.error("短链接访问量统计异常", ex);
