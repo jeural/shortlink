98a6a119920c003531a3e5ac4ec84d627daea890
jeural <jijihecaoxiaoye@gmail.com>
2023-11-30 19:30:10 +0800

    创建短链接访问统计初始化0

--- project/src/main/java/com/jane/shortlink/project/service/Impl/ShortLinkServiceImpl.java
+++ project/src/main/java/com/jane/shortlink/project/service/Impl/ShortLinkServiceImpl.java
@@ -101,6 +101,9 @@ public class ShortLinkServiceImpl extends ServiceImpl<ShortLinkMapper, ShortLink
                 .describe(requestParam.getDescribe())
                 .shortUri(shortLinkSuffix)
                 .enableStatus(0)
+                .totalUv(0)
+                .totalUv(0)
+                .totalUip(0)
                 .fullShortUrl(fullShortUrl)
                 .favicon(getFavicon(requestParam.getOriginUrl()))
                 .build();
